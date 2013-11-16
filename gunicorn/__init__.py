cd3bdd09263f7e7a4b8ffd2c39e6442bdda0ec34
denmark <denmark@chairish.com>
2013-11-15 22:58:18 -0800

    make SERVER_SOFTWARE configurable

--- gunicorn/__init__.py
+++ gunicorn/__init__.py
@@ -2,7 +2,8 @@
 #
 # This file is part of gunicorn released under the MIT license.
 # See the NOTICE for more information.
+import os
 
 version_info = (18, 0)
 __version__ = ".".join([str(v) for v in version_info])
-SERVER_SOFTWARE = "gunicorn/%s" % __version__
+SERVER_SOFTWARE = os.getenv("SERVER_SOFTWARE", "gunicorn/%s" % __version__)
